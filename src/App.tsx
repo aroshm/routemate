cc1a312858405c48a0d22d9d53b3ff3d844e3d12
Arosh Athukorala <aroshmathukorala@gmail.com>
2026-04-05 19:41:11 +0530

    add router and basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-
+import { Routes, Route } from "react-router";
 import "./App.scss";
+import Home from "./components/Home";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="products" element={<ProductList />}></Route>
+        <Route path="products/0001" element={<ProductDetails />}></Route>
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
